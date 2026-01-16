8392c2f5c5608f3161c7261b74d03d65f80e2266
nalahatra-rabe <fitiavananalahatra@gmail.com>
2026-01-16 20:52:09 +0300

    feat: create  main heading section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
 export default function Home() {
-  return <div className="flex"></div>;
+  return (
+    <section className="mx-auto flex max-w-6xl flex-col py-16 border justify-center items-center">
+      <div className="bg-white flex flex-row rounded-full px-10 py-5 w-fit  gap-4 items-center justify-center">
+        <Image
+          src={"/images/search-logo.png"}
+          alt="seach-logo"
+          width={56}
+          height={56}
+          className="h-14 w-14 shrink-0"
+        />
+        <p className="font-extrabold text-secondary text-xl">
+          CRÉER UN SITE WEB VRAIMENT UNIQUE
+        </p>
+      </div>
+    </section>
+  );
 }
